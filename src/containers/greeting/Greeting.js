ac5e71d21d38e8b30ac5062899a6da302d47ca4b
Augusto Freire <augusto9@byu.edu>
2026-03-16 20:52:11 -0600

    feat: update downloadable resume file

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,8 +41,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./Augusto Freire - Resume.pdf")}
+                    download="Augusto Freire - Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
